14db5f328f6a126b29f6f352e281c592b44d7509
Christian Frattini <chrifrat1@gmail.com>
2024-10-14 11:58:47 +0100

    setup and admin dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,78 @@
+import React, { ReactNode } from "react";
+import DashboardNavigation from "../components/dashboard/DashboardNavigation";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { CircleUserIcon, MenuIcon } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <div
+      className={"flex w-full flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"}
+    >
+      <header
+        className={
+          "sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white"
+        }
+      >
+        <nav
+          className={
+            "hidden font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-lg lg:gap-6"
+          }
+        >
+          <DashboardNavigation />
+        </nav>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              className={"shrink-0 md:hidden"}
+              variant={"outline"}
+              size={"icon"}
+            >
+              <MenuIcon className={"w-5 h-5"} />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent side={"left"}>
+            <SheetTitle>Menu</SheetTitle>
+            <SheetDescription>Admin section</SheetDescription>
+            <nav className={"grid gap-6 text-lg font-medium mt-7"}>
+              <DashboardNavigation />
+            </nav>
+          </SheetContent>
+        </Sheet>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant={"secondary"}
+              size={"icon"}
+              className={"rounded-full"}
+            >
+              <CircleUserIcon className={"h-5 w-5"} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align={"end"}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </header>
+    </div>
+  );
+}
