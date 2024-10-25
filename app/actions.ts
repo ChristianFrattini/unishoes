fe10cde10751d97fbc20e1f4598dd1f70c7a4f16
Christian Frattini <chrifrat1@gmail.com>
2024-10-25 01:57:38 +0100

    added prod url

--- app/actions.ts
+++ app/actions.ts
@@ -266,8 +266,14 @@ export async function checkOut() {
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
       line_items: lineItems, //line items is sued in the create new stripe session
-      success_url: "http://localhost:3000/payment/success",
-      cancel_url: "http://localhost:3000/payment/cancel",
+      success_url:
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:3000/payment/success"
+          : "https://unishoes.vercel.app/payment/success",
+      cancel_url:
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:3000/payment/cancel"
+          : "https://unishoes.vercel.app/payment/cancel",
       metadata: {
         userId: user.id,
       },
